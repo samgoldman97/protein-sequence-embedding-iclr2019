879edb6d06795d6ad05e9dce5f66b5fbfca28a5b
Sam Goldman <samgoldman97@gmail.com>
2020-07-02 17:01:55 -0400

    Modify the embedding util file to take a state dict rather than a saved model for the emebedding

--- bepler_embedding/embed_utils.py
+++ bepler_embedding/embed_utils.py
@@ -37,6 +37,9 @@ except ImportError:
 DEFAULT_CACHE_PATH = os.path.join(torch_cache_home, 'bepler_models_cache')
 MODEL_LINK  = "http://bergerlab-downloads.csail.mit.edu/bepler-protein-sequence-embeddings-from-structure-iclr2019/pretrained_models.tar.gz"
 
+# Mirror containing just the state dict
+MODEL_MIRROR_LINK= "https://www.dropbox.com/s/kvebr0c038z9bhm/bepler_SCOPCM_state_dict.pk?dl=0"
+
 # Make alphabet
 uniprot_alphabet = alphabets.Uniprot21()
 
@@ -51,6 +54,49 @@ def download_ftp(ftp_link: str, outfile: str):
     with closing(request.urlopen(ftp_link)) as r:
         with open(outfile, 'wb') as f:
             shutil.copyfileobj(r, f)
+
+#def load_pretrained_embedding(in_file : Optional[str] = None) -> nn.Module: 
+#    """ load_pretrained_embedding.
+#
+#    Load the pretrained embedding. If it doesn't exist or no in file is
+#    passed, download it.
+#
+#
+#    Args:
+#        in_file (Optional[str]) : Optional input argument
+#
+#    Return: 
+#        nn.Module
+#    pretrained_models/ssa_L1_100d_lstm3x512_lm_i512_mb64_tau0.5_lambda0.1_p0.05_epoch100.sav
+#    """
+#    # Try to get from cache path
+#    model_name = "pretrained_models/ssa_L1_100d_lstm3x512_lm_i512_mb64_tau0.5_lambda0.1_p0.05_epoch100.sav" 
+#    if in_file is None or not os.path.isfile(in_file): 
+#        pretrained_loc = os.path.join(DEFAULT_CACHE_PATH, model_name)
+#        # If the path doesn't exist, downlaod it
+#        if not os.path.isfile(pretrained_loc): 
+#
+#            # Make the directory if it doesn't exist
+#            if not os.path.isdir(DEFAULT_CACHE_PATH): 
+#                os.makedirs(DEFAULT_CACHE_PATH, exist_ok=True)
+#
+#            # Download the zipped file first
+#            temp_file = os.path.join(DEFAULT_CACHE_PATH, "temp.tar.gz")
+#            logging.info(f"Unable to find saved models, downloading to {temp_file}")
+#            download_ftp(MODEL_LINK, temp_file)
+#            logging.info(f"Done downloading. Unzipping file into {DEFAULT_CACHE_PATH}")
+#            # Unpack zip
+#            shutil.unpack_archive(temp_file, DEFAULT_CACHE_PATH)
+#            logging.info(f"Unzip complete. Removing {temp_file}")
+#            os.remove(temp_file)
+#        if not os.path.isfile(pretrained_loc): 
+#            raise ValueError(f"After download, still unable to load {pretrained_loc}")
+#    else: 
+#        pretrained_loc = in_file
+#
+#    # Now try to load the model
+#
+#    return torch.load(pretrained_loc)
 
 def load_pretrained_embedding(in_file : Optional[str] = None) -> nn.Module: 
     """ load_pretrained_embedding.
@@ -67,35 +113,31 @@ def load_pretrained_embedding(in_file : Optional[str] = None) -> nn.Module:
     pretrained_models/ssa_L1_100d_lstm3x512_lm_i512_mb64_tau0.5_lambda0.1_p0.05_epoch100.sav
     """
     # Try to get from cache path
-    model_name = "pretrained_models/ssa_L1_100d_lstm3x512_lm_i512_mb64_tau0.5_lambda0.1_p0.05_epoch100.sav" 
+    model_name = "bepler_SCOPCM_state_dict.pk" 
     if in_file is None or not os.path.isfile(in_file): 
         pretrained_loc = os.path.join(DEFAULT_CACHE_PATH, model_name)
         # If the path doesn't exist, downlaod it
         if not os.path.isfile(pretrained_loc): 
-
             # Make the directory if it doesn't exist
             if not os.path.isdir(DEFAULT_CACHE_PATH): 
                 os.makedirs(DEFAULT_CACHE_PATH, exist_ok=True)
 
             # Download the zipped file first
-            temp_file = os.path.join(DEFAULT_CACHE_PATH, "temp.tar.gz")
             logging.info(f"Unable to find saved models, downloading to {temp_file}")
-            download_ftp(MODEL_LINK, temp_file)
-            logging.info(f"Done downloading. Unzipping file into {DEFAULT_CACHE_PATH}")
+            download_ftp(MODEL_MIRROR_LINK, model_name)
+            logging.info(f"Done downloading.") 
             # Unpack zip
-            shutil.unpack_archive(temp_file, DEFAULT_CACHE_PATH)
-            logging.info(f"Unzip complete. Removing {temp_file}")
-            os.remove(temp_file)
+            #shutil.unpack_archive(temp_file, DEFAULT_CACHE_PATH)
+            #logging.info(f"Unzip complete. Removing {temp_file}")
+            #os.remove(temp_file)
         if not os.path.isfile(pretrained_loc): 
             raise ValueError(f"After download, still unable to load {pretrained_loc}")
     else: 
         pretrained_loc = in_file
-
     # Now try to load the model
-
     return torch.load(pretrained_loc)
 
-def remake_pretrained_model(model : nn.Module) -> nn.Module: 
+def remake_pretrained_model(state_dict : dict) -> nn.Module: 
     """ remake_pretrained_model.
 
     This function hard codes the parameters used for the pretrained bepler
@@ -103,7 +145,7 @@ def remake_pretrained_model(model : nn.Module) -> nn.Module:
     goal of loading the state dict into the most recent version of pytorch. 
 
     Args: 
-        model : Should be a SCOPCM model as uploaded and saved 
+        stat_dict: This should be an ODict containing the embeddinig state dict 
     Return: 
         nn.Module
 
@@ -125,7 +167,7 @@ def remake_pretrained_model(model : nn.Module) -> nn.Module:
     embedding_module = embedding.StackedRNN(ntokens, 512, 512, 100, 
                                             nlayers=3, dropout=0, lm=bilm)
 
-    embedding_module.load_state_dict(model.embedding.state_dict())
+    embedding_module.load_state_dict(state_dict)
     new_model = embedding_module
     return new_model
 
@@ -136,6 +178,7 @@ def get_default_saved_model() -> nn.Module:
         nn.Module: Default embedding module. This is of type stacked_RNN 
     """
     old_model = load_pretrained_embedding()
+    #new_model = remake_pretrained_model(old_model.embedding.state_dict())
     new_model = remake_pretrained_model(old_model)
     return new_model
 
